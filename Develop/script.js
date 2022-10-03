33bec0da64e79113ebb1d931a9680a1a53f334bc
Jonah1527 <112822661+Jonah1527@users.noreply.github.com>
2022-10-02 21:50:00 -0600

    few things starting the project

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,50 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "0123456789";
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
+var passwordLength;
+var uppercaseCheck;
+var numberCheck;
+var specialCheck;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//Function used to determine the length of the password
+function determineLength(){
+  passwordLength = prompt("How long would you like your password to be? (between 8-128 characters): ");
 
-  passwordText.value = password;
-
+    if (passwordLength<8){
+      alert("Password needs to be a number between 8-128 characters");
+      determineLength();
+    }else if (passwordLength>128){
+      alert("Password needs to be a number between 8-128 characters");
+      determineLength();
+    }else if (isNaN(passwordLength)){
+      alert("Password needs to be a number between 8-128 characters");
+      determineLength();
+    }
+    return passwordLength;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//Function used to determine whether the user wants to include uppercase characters in the password
+function determineUppercase(){
+  uppercaseCheck = prompt("Do you want to include uppercase letters in your password? \n(Yes or No)");
+    uppercaseCheck = uppercaseCheck.toLowerCase();
+
+    if (uppercaseCheck === null || uppercaseCheck === ""){
+      alert("Please answer Yes or No");
+      determineUppercase();
+
+    }else if (uppercaseCheck === "yes" || uppercaseCheck ==="y"){
+      uppercaseCheck = true;
+      return uppercaseCheck;
+
+    }else if (uppercaseCheck === "no" || uppercaseCheck ==="n"){
+      uppercaseCheck = false;
+      return uppercaseCheck;
+    
+    }else {
+      alert("Please answer Yes or No");
+      determineUppercase();
+    }
+    return uppercaseCheck;
+}
